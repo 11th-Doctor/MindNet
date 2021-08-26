8aa3a7400989a56bde94e94665f5e858a5c7c927
Daryl Zhang <august9382@gmail.com>
2021-08-27 01:30:41 +0800

    feat: Added horizontal collection view

--- socialApp/Views/HomeHeader.swift
+++ socialApp/Views/HomeHeader.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class HomeHeader: UICollectionReusableView {
     
+    let cellId = "cellId"
+    
     let titleLabel: UILabel = {
         let label = UILabel()
         label.text = "追蹤中"
         label.font = UIFont.boldSystemFont(ofSize: 16)
         return label
+    }()
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        let view = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        view.backgroundColor = .white
+        view.showsHorizontalScrollIndicator = false
+        return view
     }()
     
     let separatorView: UIView = {
@@ -25,16 +36,24 @@ class HomeHeader: UICollectionReusableView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
-        
     }
     
     fileprivate func setupViews() {
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        
         backgroundColor = .white
         
         addSubview(titleLabel)
+        addSubview(collectionView)
         addSubview(separatorView)
         
         titleLabel.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 16, paddingLeft: 16, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        collectionView.anchor(top: titleLabel.bottomAnchor, left: titleLabel.leftAnchor, bottom: separatorView.topAnchor, right: rightAnchor, paddingTop: 8, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
         
         separatorView.anchor(top: nil, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 1)
     }
@@ -42,4 +61,31 @@ class HomeHeader: UICollectionReusableView {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+}
+
+extension HomeHeader: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+}
+
+extension HomeHeader: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 50, height: 50)
+    }
+}
+
+extension HomeHeader: UICollectionViewDelegate {
+    
 }
